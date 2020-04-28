afe519d5a9b5bf619c5991c2ebc60f7c25ecec3f
Maurice Kleine <info@mauricekleine.com>
2020-04-28 12:52:46 +0200

    remote purge css because of dynamic styles

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,5 @@ module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: process.env.NODE_ENV === "production" ? {} : false,
-    "@fullhuman/postcss-purgecss":
-      process.env.NODE_ENV === "production"
-        ? {
-            content: ["./src/**/*.tsx"],
-            defaultExtractor: (content) =>
-              content.match(/[\w-/:]+(?<!:)/g) || [],
-          }
-        : false,
   },
 };
