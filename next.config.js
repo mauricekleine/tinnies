88a80861b1bcaa962993d7042a2038b93eea813b
Maurice Kleine <info@mauricekleine.com>
2020-05-04 12:20:55 +0200

    add rollbar on the client

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  env: {
+    ROLLBAR_CLIENT_ACCESS_TOKEN: process.env.ROLLBAR_CLIENT_ACCESS_TOKEN,
+  },
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { config } from "@fortawesome/fontawesome-svg-core";
 import { AppProps } from "next/app";
 import Head from "next/head";
 import React from "react";
+import Rollbar from "rollbar";
 
 import Footer from "../components/Footer";
 import Navigation from "../components/Navigation";
@@ -12,6 +13,14 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./styles.css";
 
 config.autoAddCss = false;
+
+if (process.env.ROLLBAR_CLIENT_ACCESS_TOKEN) {
+  new Rollbar({
+    accessToken: process.env.ROLLBAR_CLIENT_ACCESS_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+  });
+}
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   const { isLoading, isRedirecting, user } = useAuthentication();
